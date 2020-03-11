8102bd68cc3c539d5d3429c1f2eaf1e437619219
Hannah Rogers <rog19008@byui.edu>
2020-03-11 11:45:20 -0600

    Week 10 update P.1

---
+++ cit230/lesson10/js/weatherapi.js
@@ -0,0 +1,15 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=29532d73c747db9a9cb1be86effaaac6";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    
+  });
+
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
